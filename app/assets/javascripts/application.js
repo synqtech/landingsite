d7bed8d288037b4a7cb0a1ee53d09d8d3adeeaed
Quintan Neville <quintan@synqtech.com>
2014-10-28 16:30:43 -0700

    preview push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 
 $(document).ready(function() {
-	alert("hi");
+
 })
+
