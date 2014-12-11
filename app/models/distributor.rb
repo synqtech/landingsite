5e23cbe08fff7eb1c755ca327daabbcdebd68767
Quintan Neville <quintan@synqtech.com>
2014-12-10 16:53:39 -0800

    spelling error fix

--- app/models/distributor.rb
+++ app/models/distributor.rb
@@ -1,2 +1,8 @@
 class Distributor < ActiveRecord::Base
+
+	validates_presence_of :email
+	validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "is invalid" }
+	validates_format_of :phone, 
+    :message => "must be a valid telephone number.",
+    :with => /^[\(\)0-9\- \+\.]{10,20} *[extension\.]{0,9} *[0-9]{0,
 end
