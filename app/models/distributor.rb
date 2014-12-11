c79d9194b4d2217f880bf38e6a79936c0b04f220
Quintan Neville <quintan@synqtech.com>
2014-12-10 17:01:08 -0800

    push

--- app/models/distributor.rb
+++ app/models/distributor.rb
@@ -2,7 +2,6 @@ class Distributor < ActiveRecord::Base
 
 	validates_presence_of :email
 	validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "is invalid" }
-	validates_format_of :phone, 
-    :message => "must be a valid telephone number.",
-    :with => /^[\(\)0-9\- \+\.]{10,20} *[extension\.]{0,9} *[0-9]{0,
+	validates_presence_of :phone
+	
 end
