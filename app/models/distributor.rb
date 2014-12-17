f4ed3c79b0f6bd4d99fbe8773597983890ee2238
Quintan Neville <quintan@synqtech.com>
2014-12-16 17:31:24 -0800

    fixed dist/new requirements

--- app/models/distributor.rb
+++ app/models/distributor.rb
@@ -1,7 +1,13 @@
 class Distributor < ActiveRecord::Base
 
+	validates_presence_of :company
 	validates_presence_of :email
+	validates_presence_of :name
 	validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "is invalid" }
+	validates_uniqueness_of :email
 	validates_presence_of :phone
+	validates_presence_of :address_1
+	validates_presence_of :province
+	validates_presence_of :country
 	
 end
