822daab16fbb1ed7516681ca70d5feaee40c8222
Quintan Neville <quintan@synqtech.com>
2015-02-20 16:51:27 -0800

    try again

--- db/migrate/20150216200006_create_roi_surveys.rb
+++ db/migrate/20150216200006_create_roi_surveys.rb
@@ -2,11 +2,11 @@ class CreateRoiSurveys < ActiveRecord::Migration
   def change
     create_table :roi_surveys do |t|
       t.integer :store_sq_ft
-      t.boolean :haba
+      t.integer :haba
       t.integer :cctv
       t.string :walking_svc
       t.boolean :existing_eas
-      t.boolean :eas_effective
+      t.integer :eas_effective
       t.integer :eas_tags_per_month
       t.integer :total_annual_sales
       t.integer :meat_sales_percent
--- db/migrate/20150220002109_change_columns_to_intsfor_roi_survey.rb
+++ db/migrate/20150220002109_change_columns_to_intsfor_roi_survey.rb
@@ -1,7 +1,7 @@
 class ChangeColumnsToIntsforRoiSurvey < ActiveRecord::Migration
   def change
 
-  	change_column :roi_surveys, :eas_effective, :integer
+  	#change_column :roi_surveys, :eas_effective, :integer
 
   end
 end
